31c4d05290641ff59a6267850cd79fb1c46189f0
Debasree97 <atd21821@gmail.com>
2021-11-11 00:24:14 +0600

    get product

--- index.js
+++ index.js
@@ -21,7 +21,28 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        console.log("connected");
+        const database = client.db("Sree'sHandicrafts");
+        const productCollection = database.collection("productsList");
+        
+        // get: all products
+        app.get("/allproduct", async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const allProducts = await cursor.toArray();
+            const homeProducts = allProducts.slice(3,9);
+            console.log(homeProducts);
+            res.send({allProducts,homeProducts});
+        })
+
+        // get: few products
+        app.get("/homeproduct", async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+           const homeProducts = await cursor
+              .limit(6)
+              .toArray();
+            res.send(homeProducts);
+        })
     }
     finally {
         // await client.close();
